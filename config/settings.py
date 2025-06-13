749cbf7805dd4deb3401c92d6b1f801487891554
hagarbo <hagarbo@gmail.com>
2025-06-13 14:23:34 +0200

    fix postgres deployment conf

--- config/settings.py
+++ config/settings.py
@@ -97,9 +97,20 @@ if os.getenv('RENDER'):
             'CONN_MAX_AGE': 600,  # 10 minutos
             'OPTIONS': {
                 'sslmode': 'require',
+                'connect_timeout': 30,
             },
         }
     }
+    
+    # Configuración de seguridad para producción
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_HSTS_SECONDS = 31536000  # 1 año
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
 else:
     # Configuración para desarrollo local
     DATABASES = {
