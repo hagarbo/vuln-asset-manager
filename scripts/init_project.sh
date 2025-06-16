91531a6c0795d11fa0d5245938ac1d0a2211519e
hagarbo <hagarbo@gmail.com>
2025-06-16 18:10:21 +0200

    fix:superuser rol

--- scripts/init_project.sh
+++ scripts/init_project.sh
@@ -18,9 +18,7 @@ django.setup()
 from django.contrib.auth import get_user_model
 User = get_user_model()
 User.objects.filter(username='admin').delete()
-admin = User.objects.create_superuser('admin', 'admin@admin.com', 'Admin123!')
-admin.rol = 'admin'
-admin.save()
+User.objects.create_superuser('admin', 'admin@admin.com', 'Admin123!', rol='admin')
 print('Superusuario creado correctamente con rol de administrador')
 END
 
